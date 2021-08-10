cebe4242308f5a3fb1c289dbb0bd576682d9bcef
Vitalii Gusakovskiy <v.s.gusakovskiy@gmail.com>
2021-08-10 19:06:55 +0300

    module_6. Add count up count down example from dabeaz asyncio

---
+++ module_6/example_up_down_block_async.py
@@ -0,0 +1,30 @@
+import asyncio
+import time
+
+
+async def count_up(count):
+    counter = 0
+    while counter < count:
+        print("Up", counter)
+        counter += 1
+        time.sleep(1)
+
+
+async def count_down(count):
+    while count > 0:
+        print("Down ", count)
+        time.sleep(1)
+        count -= 1
+
+
+async def main():
+    tasks1 = asyncio.create_task(count_up(10))
+    tasks2 = asyncio.create_task(count_down(10))
+    await tasks1
+    await tasks2
+    print(f'Result task {tasks1.result()}')
+    print(f'Result task {tasks2.result()}')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
