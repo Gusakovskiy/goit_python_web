e246a2b4fccf4ea18a3eb0762fa00152975c4ea8
Vitalii Gusakovskiy <v.s.gusakovskiy@gmail.com>
2021-08-29 17:56:33 +0200

    module_9.Fix

--- module_9/personal_assistant/queries.py
+++ module_9/personal_assistant/queries.py
@@ -1,23 +1,41 @@
-from sqlalchemy import select, func, desc, nullslast
 from datetime import datetime
 
-from sqlalchemy.orm import joinedload
+from sqlalchemy import or_, and_
+from sqlalchemy import select, func, desc, nullslast
+from sqlalchemy.orm import joinedload, load_only, selectinload
 
 from module_9.personal_assistant.db import session
-from module_9.personal_assistant.models import User, Company, Contact, company_user_table
-from sqlalchemy import or_, and_
+from module_9.personal_assistant.models import User, Company, Contact
 
 
-def user_with_contacts():
-    # https://docs.sqlalchemy.org/en/13/orm/loading_relationships.html
-    statement = session.query(User).options(joinedload('contacts'))
-
-    result = session.execute(statement).scalars()
-    for user in result:
+def user_with_contacts_join():
+    users = session.query(User).options(joinedload('contacts'))
+    # Redundant and should not be here
+    # result = session.execute(users).scalars()
+    for user in users:
         print(user.full_name)
         for c in user.contacts:
             print(c.email, c.cell_phone)
         print('')
+
+
+def user_with_contacts_proper_join():
+    # https://docs.sqlalchemy.org/en/13/orm/loading_relationships.html
+    # https://docs.sqlalchemy.org/en/14/orm/loading_columns.html
+    users = session.query(
+         User
+    ).join(
+        User.contacts
+    ).options(
+        # joinedload(User.contacts).load_only(Contact.email, Contact.cell_phone),
+        selectinload(User.contacts).load_only(Contact.email, Contact.cell_phone),
+        load_only(User.user_id, User.first_name, User.last_name),
+    )
+    for user in users:
+        print(user.full_name)
+        for c in user.contacts:
+            print(c.email, c.cell_phone)
+    print('')
 
 
 def contact_with_birthday():
@@ -63,7 +81,8 @@ def most_working_person():
 
 
 if __name__ == '__main__':
-    user_with_contacts()
+    user_with_contacts_join()
+    # user_with_contacts_proper_join()
     # contact_with_birthday()
     # most_working_person()
 
