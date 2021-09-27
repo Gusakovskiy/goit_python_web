14f3ae3a6d1c91b120a3e0bbd48946ce3b571125
Vitalii Gusakovskiy <v.s.gusakovskiy@gmail.com>
2021-09-27 22:48:40 +0200

    module_13. Add user

---
+++ personal_finance/account/permission.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from rest_framework.permissions import BasePermission
+
+
+class IsSameUserPermission(BasePermission):
+
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        user: User = request.user
+        # allow for view if user not anonymous
+        # and it is same
+        return (
+            not user.is_anonymous and
+            user.id == obj.id  # NOQA
+        )
